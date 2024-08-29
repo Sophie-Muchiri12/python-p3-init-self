7d99b7b7c94c9e41e70a7e7dd90ba8f7a87bc27b
Sophie Muchiri <muthonisophie12@gmail.com>
2024-08-29 23:25:05 +0300

    Init self

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name,breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+
+
+dog = Dog('fido')
+
+
+class Person:
+    def __init__(self,name):
+        self.name = name
+
+
+
